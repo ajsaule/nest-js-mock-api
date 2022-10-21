3103938127e9c5c8cbe549ad3fed80afe2dc4fae
ajsaule <andrejsaule8@gmail.com>
2022-10-21 17:55:15 +1100

    first commit

---
+++ src/config/keys.ts
@@ -0,0 +1,4 @@
+export default {
+  mongoURI:
+    'mongodb+srv://ajsaule:andrej1234@cluster0.3gsafyv.mongodb.net/?retryWrites=true&w=majority',
+};
---
+++ src/items/items.controller.ts
@@ -0,0 +1,51 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Req,
+  Res,
+  Param,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+import { CreateItemDto } from './dto/create-item.dto';
+import { ItemsService } from './items.service';
+import { Item } from './interfaces/item.interface';
+
+@Controller('items')
+export class ItemsController {
+  // Injecting the dependency exactly like it is in Angular
+  constructor(private readonly itemsService: ItemsService) {}
+
+  // @Get()
+  // findAll(@Req() req: Request, @Res() res: Response): Response {
+  //   console.log(req.url);
+  //   return res.send('Hello world');
+  // }
+  @Get()
+  findAll(): Promise<Item[]> {
+    return this.itemsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id): Promise<Item> {
+    return this.itemsService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() createItemDto: CreateItemDto): Promise<Item> {
+    return this.itemsService.create(createItemDto);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id): Promise<Item> {
+    return this.itemsService.delete(id);
+  }
+
+  @Put(':id')
+  update(@Body() updateItemDto: CreateItemDto, @Param('id') id): Promise<Item> {
+    return this.itemsService.update(id, updateItemDto);
+  }
+}
